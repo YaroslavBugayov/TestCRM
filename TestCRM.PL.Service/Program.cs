af15bdec4c0ee15d4027369fe91967adf3dae609
YaroslavBugayov <bugayov576@gmail.com>
2026-02-06 18:39:09 +0200

    feature: Add db connection functionality

--- TestCRM.PL.Service/Program.cs
+++ TestCRM.PL.Service/Program.cs
@@ -1,7 +1,10 @@
+using TestCRM.DAL.Data;
 using TestCRM.PL.Service;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton<DapperContext>();
 
 var host = builder.Build();
 host.Run();
